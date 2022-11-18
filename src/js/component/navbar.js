77b11776ab0175159cac1892c018635c1df216a7
agosfasanelli <agosfasanelli@gmail.com>
2022-11-18 11:05:42 +0000

    bhasgdhsa

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -3,15 +3,26 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
+		<div className="col-12">
+			<div className="row">
+				<nav className="navbar navbar-light bg-light mb-3">
+					<Link to="/">
+						<span className="navbar-brand m-3 d-inline-flex ">
+						<a href="https://3000-4geeksacade-reacthellow-jkp6s4xo261.ws-eu74.gitpod.io/">
+							<img src="https://www.aaddpa.org/ckfinder/userfiles/files/starwars.png" 
+							alt="MDN"
+							className="m-3 col-md-4 col-2"
+							/>
+						</a>
+						</span>
+					</Link>
+					<div className="d-inline-flex">
+						<Link to="/demo">
+							<button className="btn btn-warning col-md-6 col-4">Favoritos</button>
+						</Link>
+					</div>
+				</nav>
 			</div>
-		</nav>
+		</div>
 	);
 };
