fc629f96bb3161326788e9d0754fb215bedde791
agosfasanelli <agosfasanelli@gmail.com>
2022-11-18 11:24:57 +0000

    jbsajbhda

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -3,26 +3,21 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<div className="col-12">
-			<div className="row">
-				<nav className="navbar navbar-light bg-light mb-3">
-					<Link to="/">
-						<span className="navbar-brand m-3 d-inline-flex ">
-						<a href="https://3000-4geeksacade-reacthellow-jkp6s4xo261.ws-eu74.gitpod.io/">
-							<img src="https://www.aaddpa.org/ckfinder/userfiles/files/starwars.png" 
-							alt="MDN"
-							className="m-3 col-md-4 col-2"
-							/>
-						</a>
-						</span>
-					</Link>
-					<div className="d-inline-flex">
-						<Link to="/demo">
-							<button className="btn btn-warning col-md-6 col-4">Favoritos</button>
-						</Link>
-					</div>
-				</nav>
+		<nav className="navbar navbar-light bg-light mb-3">
+			<Link to="/home">
+				<span className="navbar-brand mb-0 h1">
+				<img 
+					className="icono m-3"
+					src="https://www.aaddpa.org/ckfinder/userfiles/files/starwars.png" 
+					alt="MDN"/>
+				</span>
+			</Link>
+			<div className="ml-auto">
+				<Link to="/demo">
+					<button className="btn btn-warning m-3">Favoritos</button>
+				</Link>
 			</div>
-		</div>
+		</nav>
+				
 	);
 };
